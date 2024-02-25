134dcca2ea7aec0d0eb9a2fd8ac412bef86c967e
hosein khaleghi <hoseinkhaleghi97@gmail.com>
2024-02-25 23:15:54 +0330

    update crud

--- src/pages/landingpage/Landingpage.tsx
+++ src/pages/landingpage/Landingpage.tsx
@@ -1,13 +1,9 @@
-import Header from "../../components/features/landingpage/Header";
 import Main from "../../components/features/landingpage/Main";
-import Slider from "../../components/features/landingpage/Slider";
 
 function Landingpage() {
     
   return (
     <div>
-      <Header />
-      <Slider />
       <Main />
     </div>
   );
